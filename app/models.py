1d88dc41f689e422bc2f46c7e864610bbaba9936
Ignacio Barroso <ignacio_barroso@outlook.com>
2021-12-07 05:40:06 -0300

    Functional

--- app/models.py
+++ app/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=200, null=True)
     completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.title
--- app/views.py
+++ app/views.py
@@ -14,18 +14,44 @@ def index(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')    
+            context = {'tasks': tasks} 
+            return redirect('/')   
+        context = {'tasks': tasks} 
     
-    context = {'tasks': tasks}
+    if request.method == 'GET':
+        date = request.GET.get('date')
+        word = request.GET.get('word')
+        
+        if word and date:
+            tasks = tasks.filter(created_at=date, title__contains=word)
+            context = {'tasks': tasks}
+            return render(request, 'index.html', context)
+        
+        if date:
+            tasks = tasks.filter(created_at=date)
+            context = {'tasks': tasks}
+            print(tasks)
+            return render(request, 'index.html', context)
+        
+        if word:
+            tasks = tasks.filter(title__contains=word)
+            context = {'tasks': tasks}
+            return render(request, 'index.html', context)
+        
+    context = {'tasks': tasks} 
     return render(request, 'index.html', context)
 
 @csrf_exempt
 def delete(request, id):
     Task.objects.filter(id=id).delete()
-    return JsonResponse({'message': "Success"}, status=200)
+    return redirect('/')
 
 @csrf_exempt
 def update_status(request, id):
-    jd = json.loads(request.body)
-    Task.objects.filter(id=id).update(completed=jd['completed'])
-    return JsonResponse({'message': "Success"}, status=200)
+    task_status = (Task.objects.filter(id=id))[0].completed
+    task = Task.objects.filter(id=id)
+    if task_status == True:
+        task.update(completed=False)
+    else:
+        task.update(completed=True)
+    return redirect('/')
